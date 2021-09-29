57e84ef5c5e778b20e9035d7907d60eebb5e6ec9
Leonid Podriz <leonidpodriz@gmail.com>
2021-09-29 21:03:39 +0300

    :white_check_mark: Add mock.patch example test in "Solve me first" task `tests.py`

--- solve_me_first/tests.py
+++ solve_me_first/tests.py
@@ -1,12 +1,20 @@
 from unittest import TestCase
+from unittest.mock import MagicMock, patch
 
 from solve_me_first.task import solve_me_first
 
 
 class SolveMeFirstTest(TestCase):
 
-    def test_function_1(self):
-        self.assertEqual(solve_me_first(2, 3), 5)
+    def test_function_1(self) -> None:
+        self.assertEqual(5, solve_me_first(2, 3))
 
-    def test_function_2(self):
-        self.assertEqual(solve_me_first(4, 6), 10)
+    def test_function_2(self) -> None:
+        self.assertEqual(10, solve_me_first(4, 6))
+
+    @patch('builtins.sum', return_value=10)
+    def test_function_3_with_mock_example(self, mocked_sum: MagicMock) -> None:
+        self.assertEqual(0, mocked_sum.call_count)
+        self.assertEqual(10, solve_me_first(4, 6))
+        self.assertEqual(1, mocked_sum.call_count)
+        mocked_sum.assert_called_once_with((4, 6))
